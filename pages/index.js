ae3770781475eb58954ece439bf3acc467691f83
Omate23 <ordody.mate@gmail.com>
2020-08-07 09:58:27 +0200

    vagzok vagyok

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ allPostsData }) {
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p>Omate vagzok</p>
+				<p>Omate vagyok</p>
 				<p>
 					(This is a sample website - you’ll be building a site like this on{' '}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
